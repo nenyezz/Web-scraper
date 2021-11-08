a8050604118f47ae232192e338bc4dd4399dffdd
Chinenye Obasi <88116521+nenyezz@users.noreply.github.com>
2021-11-08 15:15:55 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const PORT = 3002
+const axios = require('axios')
+const cheerio = require('cheerio')
+const express = require('express')
+
+
+const app = express()
+const url = 'https://punchng.com/'
+
+axios(url)
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const articles = []
+
+
+        $('.entry-title', html).each(function() {
+            const title = $(this).text()
+            const url = $(this).find('a').attr('href')
+            articles.push({ 
+                title,
+                url
+            })
+        })
+        console.log(articles);
+    }).catch(err => console.log(err))
+
+app.listen(PORT, () => console.log('server listening on PORT ${PORT}') )
+
